d1ab1b059b015cc5d87875458d7e71e77d3de8f0
Ethan <101267182+EthanUltra@users.noreply.github.com>
2022-09-26 15:36:40 +0200

    Added more changes, files, etc.

--- Front-End/Projekt/lb_294_front_end/src/App.js
+++ Front-End/Projekt/lb_294_front_end/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import BookList from "./components/BookList";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Favourites from './components/Favourites';
+import BookDetails from './components/BookDetails';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookList />} /> 
+        <Route path="/books/:id" element={<BookDetails />} /> 
+        <Route path="/favourites" element={<Favourites />} /> 
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- Front-End/Projekt/lb_294_front_end/src/components/Navbar.js
+++ Front-End/Projekt/lb_294_front_end/src/components/Navbar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../App';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div>
+            <h1>React Books App</h1>
+        </div>
+        <div>
+            <Link to="/favourites"><h3>Your Favourites</h3></Link>
+        </div>
+    </div>
   )
 }
 
