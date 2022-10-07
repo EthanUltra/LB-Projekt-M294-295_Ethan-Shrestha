e38db48d80f647befd601add072730c0574028bb
Ethan <101267182+EthanUltra@users.noreply.github.com>
2022-10-07 10:35:13 +0200

    Added changes

--- Back-End/webshop/src/main/java/ch/wiss/webshop/controller/BookController.java
+++ Back-End/webshop/src/main/java/ch/wiss/webshop/controller/BookController.java
@@ -1,5 +1,18 @@
 package ch.wiss.webshop.controller;
 
+import java.sql.*;
+import java.sql.SQLException;
+
 public class BookController {
 
+	static final String DB_URL = "jdbc:mysql://localhost:3307/LB_WebshopDatenbank";
+	static final String USER = "LB_Webshop";
+	static final String PASS = "passwd";
+	
+	public static void main(String[] args) {
+	BookController test = new BookItemController();
+	try {
+		test.addGameBookItem()
+	}
+	}
 }
