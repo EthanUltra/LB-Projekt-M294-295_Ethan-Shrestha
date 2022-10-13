3087fe9193f8cb110baafe9671be5bc227ab172c
Ethan <101267182+EthanUltra@users.noreply.github.com>
2022-10-13 12:13:18 +0200

    Updated Webshop

--- Back-End/webshop/src/main/java/ch/wiss/webshop/WebshopApplication.java
+++ Back-End/webshop/src/main/java/ch/wiss/webshop/WebshopApplication.java
@@ -1,27 +1,17 @@
 package ch.wiss.webshop;
 
-import java.util.ArrayList;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RestController
+@RestController 
 @SpringBootApplication
 public class WebshopApplication {
 	
-	ArrayList<String> items = new ArrayList<>();
-	
-	@Bean
+	@Bean // This is to allow access from Back-End to Front-End by allowing Origin (CORS).
 	public WebMvcConfigurer configure() {
 		return new WebMvcConfigurer() {
 			@Override
@@ -31,28 +21,8 @@ public class WebshopApplication {
 		};
 	}
 	
+	// To start SpringBoot.
 	public static void main(String[] args) {
 		SpringApplication.run(WebshopApplication.class, args);
 	}
-	
-	@GetMapping("/")
-	public String index() {
-		return "Welcome to the Webshop for books! ";
-	}
-	
-	@GetMapping("/items")
-	public @ResponseBody Iterable<String> items() {
-		return items;
-	}
-
-	@PostMapping("/item")
-	public String addItem(@RequestParam String item) {
-		items.add(item);
-		return "saved";
-	}
-
-	@DeleteMapping("/item")
-	public String deleteItem(@RequestParam String item) {
-		return String.valueOf(items.remove(item));
-	}	
 }
