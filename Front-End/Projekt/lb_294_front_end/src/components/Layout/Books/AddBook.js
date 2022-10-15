da05571c0fd6142a3e82f5d136689c25c2cb68fe
Ethan <101267182+EthanUltra@users.noreply.github.com>
2022-10-15 10:53:40 +0200

    Final Change

--- Front-End/Projekt/lb_294_front_end/src/components/Layout/Books/AddBook.js
+++ Front-End/Projekt/lb_294_front_end/src/components/Layout/Books/AddBook.js
@@ -11,16 +11,11 @@ export default function AddBook() {
         id: "",
         title: "",
         authors: "",
-        description: "",
-        numPages: "",
-        rating: "",
-        rating_Count: "",
-        review_Count: "",
-        genre: ""
+        description: ""
     })
 
 
-    const { id, title, authors, description, numPages, rating, rating_Count, review_Count, genres } = book
+    const { id, title, authors, description } = book
 
     const onInputChange = (e) => {
         setBook({ ...book, [e.target.name]: e.target.value });
@@ -68,46 +63,6 @@ export default function AddBook() {
                         placeholder="description..."
                         name="description"
                         value={description}
-                        onChange={(e) => onInputChange(e)}
-                    />
-                    <label>Number of Pages</label>
-                    <input
-                        type={"text"}
-                        placeholder="Number of Pages..."
-                        name="numPages"
-                        value={numPages}
-                        onChange={(e) => onInputChange(e)}
-                    />
-                    <label>Rating (with decimals)</label>
-                    <input
-                        type={"text"}
-                        placeholder="Rating from 1-5..."
-                        name="rating"
-                        value={rating}
-                        onChange={(e) => onInputChange(e)}
-                    />
-                    <label>Rating Count</label>
-                    <input
-                        type={"text"}
-                        placeholder="Rating Count..."
-                        name="rating_Count"
-                        value={rating_Count}
-                        onChange={(e) => onInputChange(e)}
-                    />
-                    <label>Review Count</label>
-                    <input
-                        type={"text"}
-                        placeholder="Review Count..."
-                        name="review_Count"
-                        value={review_Count}
-                        onChange={(e) => onInputChange(e)}
-                    />
-                    <label>Genre</label>
-                    <input
-                        type={"text"}
-                        placeholder="Genre..."
-                        name="genres"
-                        value={genres}
                         onChange={(e) => onInputChange(e)}
                     />
                     <button type="submit">Submit</button>
